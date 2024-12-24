97be1c76892a29df78e4c8431158dbe91fe851cb
Vladyslav Nosal <vladyslavnosal@gmail.com>
2024-12-24 11:46:30 -0800

    Add esbuild.wasm as static asset in Vite config

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,14 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
+// https://vite.dev/config/-wasm/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+  ],
+  assetsInclude: [
+    "node_modules/esbuild-wasm/**/*.wasm",
   ],
   resolve: {
     alias: {
